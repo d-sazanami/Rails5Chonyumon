b97cfdeb23ff2fb15ced9e19c09baf40e8c6cd33
t-sagiri <sagirix.t@gmail.com>
2021-05-18 09:06:43 +0900

    メッセージ詳細表示実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #patch 'messages/edit/:id', to: 'messages#update'
 
   #get 'messages/delete/:id', to: 'messages#delete'
-  #get 'messages/:id', to: 'messages#show'
+  get 'messages/:id', to: 'messages#show'
 
   get 'cards/index'
   get 'cards', to: 'cards#index'
