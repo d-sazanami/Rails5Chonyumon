2a6149da151245ca3de9b83f205ce550fed59d2f
t-sagiri <sagirix.t@gmail.com>
2021-05-29 14:51:43 +0900

    集計結果表示機能作成

--- app/controllers/questionary_results_controller.rb
+++ app/controllers/questionary_results_controller.rb
@@ -12,6 +12,27 @@ class QuestionaryResultsController < ApplicationController
     @questionary_results = QuestionaryResult.where('questionary_id = ?', params[:id])
   end
 
+  def calc
+    @questionary = Questionary.find params[:id]
+    results = QuestionaryResult.where('questionary_id = ?', params[:id])
+    @calc = {}
+
+    results.each do |result|
+      data = result.result.split ','
+        data.each do |value|
+        keyval = value.split ':'
+        ky = keyval[0].to_s
+        vl = keyval[1].to_i
+        if ky != 'questionary_id' then
+          if @calc[ky] == nil then
+            @calc[ky] = []
+          end
+          @calc[ky][vl] = @calc[ky][vl] == nil ? 1 : @calc[ky][vl].to_i + 1
+        end
+      end
+    end
+  end
+
   # GET /questionary_results/new
   def new
     @questionary_result = QuestionaryResult.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'questionary_results/calc/:id', to:'questionary_results#calc'
   get 'questionary_choices/new/:id', to: 'questionary_choices#new'
   get 'questionary_items/new/:id', to: 'questionary_items#new'
   post 'questionaries/:id', to: 'questionaries#sendform'
