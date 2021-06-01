d49a2cd53fa5af15d860f82823a2a7375f1756e5
t-sagiri <sagirix.t@gmail.com>
2021-06-01 14:15:23 +0900

    Yahoo!ニュースのRSSからajaxで情報を表示する…ができない
    - Yahoo側でHTTP Status 301を返してきて、XMLレスポンスが取得できない

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 class AjaxController < ApplicationController
   layout 'ajax'
 
@@ -5,8 +8,10 @@ class AjaxController < ApplicationController
   end
 
   def data
-    @data = Person.where 'new like ?', '%' + params[:name] + '%'
-    @data = @data.to_json.html_safe
+    url = URI('https://news.yahoo.co.jp/rss/topics/top-picks.xml')
+    http = Net::HTTP.new(url.host)
+    response = http.get(url)
+    @data = Hash.from_xml(response.body).to_json.html_safe
   end
 
 end
