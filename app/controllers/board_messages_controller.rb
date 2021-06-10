0ccde0133e055079960bbffcdc6ff28e223ce554
t-sagiri <sagirix.t@gmail.com>
2021-06-10 10:36:48 +0900

    伝言板メッセージ登録機能作成

--- app/controllers/board_messages_controller.rb
+++ app/controllers/board_messages_controller.rb
@@ -1,34 +1,50 @@
 class BoardMessagesController < ApplicationController
   before_action :set_board_message, only: %i[ show edit update destroy ]
+  before_action :authenticate_account!
 
   # GET /board_messages or /board_messages.json
   def index
-    @board_messages = BoardMessage.all
+    @board_messages = BoardMessage.page(params[:page]).order('created_at desc')
+    users = BoardUser.where 'account_id = ?', current_account.id.to_s
+    if users[0] == nil then
+      user = BoardUser.new
+      user.account_id = current_account.id.to_s
+      user.nicname = '<<no name>>'
+      user.save
+      users = BoardUser.where 'account_id = ?', current_account.id.to_s
+    end
+    @board_user = users[0]
+    @board_message = BoardMessage.new
+    @board_message.board_user_id = @board_user.id
   end
 
   # GET /board_messages/1 or /board_messages/1.json
   def show
+    redirect_to 'board_messages'
   end
 
   # GET /board_messages/new
   def new
-    @board_message = BoardMessage.new
+    redirect_to 'board_messages'
   end
 
   # GET /board_messages/1/edit
   def edit
+    redirect_to 'board_messages'
   end
 
   # POST /board_messages or /board_messages.json
   def create
+    @board_messages = BoardMessage.page(params[:page]).order('created_at')
     @board_message = BoardMessage.new(board_message_params)
+    @board_user = BoardUser.where('account_id = ?', current_account.id.to_s)[0]
 
     respond_to do |format|
       if @board_message.save
-        format.html { redirect_to @board_message, notice: "Board message was successfully created." }
+        format.html { redirect_to '/board_messages' }
         format.json { render :show, status: :created, location: @board_message }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :index }
         format.json { render json: @board_message.errors, status: :unprocessable_entity }
       end
     end
@@ -36,24 +52,12 @@ class BoardMessagesController < ApplicationController
 
   # PATCH/PUT /board_messages/1 or /board_messages/1.json
   def update
-    respond_to do |format|
-      if @board_message.update(board_message_params)
-        format.html { redirect_to @board_message, notice: "Board message was successfully updated." }
-        format.json { render :show, status: :ok, location: @board_message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @board_message.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to 'board_messages'
   end
 
   # DELETE /board_messages/1 or /board_messages/1.json
   def destroy
-    @board_message.destroy
-    respond_to do |format|
-      format.html { redirect_to board_messages_url, notice: "Board message was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to 'board_messages'
   end
 
   private
